63c14282af1325780e002e5fe7002a38a8aa4a26
Vitor Christovao <vitor_christovao@outlook.com>
2021-09-13 16:25:17 -0300

    reverse user pra 19c ajustado

--- reversa_user.sql
+++ reversa_user.sql
@@ -1,4 +1,13 @@
+set long 20000 longchunksize 20000 pagesize 0 linesize 1000 feedback off verify off trimspool on
+column ddl format a1000
+
+begin
+   dbms_metadata.set_transform_param (dbms_metadata.session_transform, 'SQLTERMINATOR', true);
+   dbms_metadata.set_transform_param (dbms_metadata.session_transform, 'PRETTY', true);
+end;
+/
+
 @set
 
-select dbms_metadata.get_ddl('USER', u&1) AS
+select dbms_metadata.get_ddl('USER', upper('&1')) from dual;
 undef 1
