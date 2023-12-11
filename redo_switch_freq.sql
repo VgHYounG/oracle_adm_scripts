b02183beb4098c94fd95a61bc099f0ae010f307e
azerakih <vitor_christovao@outlook.com>
2023-12-11 10:33:32 -0300

    created script to check redo switch frequency

--- redo_switch_freq.sql
+++ redo_switch_freq.sql
@@ -1,29 +1,58 @@
-select to_char(first_time,'DD/MM/YYYY') day,
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'00',1,0)),'999') "00",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'01',1,0)),'999') "01",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'02',1,0)),'999') "02",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'03',1,0)),'999') "03",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'04',1,0)),'999') "04",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'05',1,0)),'999') "05",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'06',1,0)),'999') "06",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'07',1,0)),'999') "07",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'08',1,0)),'999') "08",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'09',1,0)),'999') "09",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'10',1,0)),'999') "10",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'11',1,0)),'999') "11",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'12',1,0)),'999') "12",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'13',1,0)),'999') "13",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'14',1,0)),'999') "14",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'15',1,0)),'999') "15",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'16',1,0)),'999') "16",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'17',1,0)),'999') "17",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'18',1,0)),'999') "18",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'19',1,0)),'999') "19",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'20',1,0)),'999') "20",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'21',1,0)),'999') "21",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'22',1,0)),'999') "22",
-to_char(sum(decode(substr(to_char(first_time,'HH24'),1,2),'23',1,0)),'999') "23",
-sum(1) "TOTAL_IN_DAY"
-from v$log_history
-group by to_char(first_time,'DD/MM/YYYY')
-order by to_date(day) desc;
+set pages 999 lines 400
+col Day form a10
+col h0 format 999
+col h1 format 999
+col h2 format 999
+col h3 format 999
+col h4 format 999
+col h5 format 999
+col h6 format 999
+col h7 format 999
+col h8 format 999
+col h9 format 999
+col h10 format 999
+col h11 format 999
+col h12 format 999
+col h13 format 999
+col h14 format 999
+col h15 format 999
+col h16 format 999
+col h17 format 999
+col h18 format 999
+col h19 format 999
+col h20 format 999
+col h21 format 999
+col h22 format 999
+col h23 format 999
+SELECT TRUNC (first_time) "Date", inst_id, TO_CHAR (first_time, 'Dy') "Day",
+COUNT (1) "Total",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '00', 1, 0)) "h0",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '01', 1, 0)) "h1",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '02', 1, 0)) "h2",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '03', 1, 0)) "h3",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '04', 1, 0)) "h4",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '05', 1, 0)) "h5",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '06', 1, 0)) "h6",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '07', 1, 0)) "h7",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '08', 1, 0)) "h8",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '09', 1, 0)) "h9",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '10', 1, 0)) "h10",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '11', 1, 0)) "h11",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '12', 1, 0)) "h12",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '13', 1, 0)) "h13",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '14', 1, 0)) "h14",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '15', 1, 0)) "h15",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '16', 1, 0)) "h16",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '17', 1, 0)) "h17",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '18', 1, 0)) "h18",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '19', 1, 0)) "h19",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '20', 1, 0)) "h20",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '21', 1, 0)) "h21",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '22', 1, 0)) "h22",
+SUM (DECODE (TO_CHAR (first_time, 'hh24'), '23', 1, 0)) "h23",
+ROUND (COUNT (1) / 24, 2) "Avg"
+FROM gv$log_history
+WHERE thread# = inst_id
+AND first_time > sysdate -7
+GROUP BY TRUNC (first_time), inst_id, TO_CHAR (first_time, 'Dy')
+ORDER BY 1,2;
